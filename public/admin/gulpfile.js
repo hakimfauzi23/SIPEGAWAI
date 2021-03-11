531abb80c9ad1a3c16eae8808454adaf39dc9caa
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-03-11 18:39:15 +0700

    Tambah Asset Template Baru

--- public/admin/gulpfile.js
+++ public/admin/gulpfile.js
@@ -1,145 +1,112 @@
-"use strict";
+/* ------------------------------------------------------------------------------
+ *
+ *  # Gulp file
+ *
+ *  Basic Gulp tasks for Limitless template
+ *
+ *  Version: 1.1
+ *  Latest update: Aug 20, 2016
+ *
+ * ---------------------------------------------------------------------------- */
 
-// Load plugins
-const autoprefixer = require("gulp-autoprefixer");
-const browsersync = require("browser-sync").create();
-const cleanCSS = require("gulp-clean-css");
-const del = require("del");
-const gulp = require("gulp");
-const header = require("gulp-header");
-const merge = require("merge-stream");
-const plumber = require("gulp-plumber");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify");
 
-// Load package.json for banner
-const pkg = require('./package.json');
+// Include gulp
+var gulp = require('gulp'); 
 
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/StartBootstrap/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
 
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000
-  });
-  done();
-}
+// Include our plugins
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var minifyCss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-// BrowserSync reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
 
-// Clean vendor
-function clean() {
-  return del(["./vendor/"]);
-}
+// Lint task
+gulp.task('lint', function() {
+    return gulp
+        .src('assets/js/core/app.js')                 // lint core JS file. Or specify another path
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
-// Bring third party dependencies from node_modules into vendor directory
-function modules() {
-  // Bootstrap JS
-  var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/*')
-    .pipe(gulp.dest('./vendor/bootstrap/js'));
-  // Bootstrap SCSS
-  var bootstrapSCSS = gulp.src('./node_modules/bootstrap/scss/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap/scss'));
-  // ChartJS
-  var chartJS = gulp.src('./node_modules/chart.js/dist/*.js')
-    .pipe(gulp.dest('./vendor/chart.js'));
-  // dataTables
-  var dataTables = gulp.src([
-      './node_modules/datatables.net/js/*.js',
-      './node_modules/datatables.net-bs4/js/*.js',
-      './node_modules/datatables.net-bs4/css/*.css'
-    ])
-    .pipe(gulp.dest('./vendor/datatables'));
-  // Font Awesome
-  var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
-    .pipe(gulp.dest('./vendor'));
-  // jQuery Easing
-  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
-  // jQuery
-  var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'));
-  return merge(bootstrapJS, bootstrapSCSS, chartJS, dataTables, fontAwesome, jquery, jqueryEasing);
-}
 
-// CSS task
-function css() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: "expanded",
-      includePaths: "./node_modules",
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest("./css"))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
-    .pipe(browsersync.stream());
-}
+// Compile less files of a full version
+gulp.task('less_full', function() {
+    return gulp
+        .src('assets/less/_main_full/*.less')         // locate /less/ folder root to grab 4 main files
+        .pipe(less())                                 // compile
+        .pipe(gulp.dest('assets/css'))                // destination path for normal CSS
+        .pipe(minifyCss({                             // minify CSS
+            keepSpecialComments: 0                    // remove all comments
+        }))
+        .pipe(rename({                                // rename file
+            suffix: ".min"                            // add *.min suffix
+        }))
+        .pipe(gulp.dest('assets/css'));               // destination path for minified CSS
+});
 
-// JS task
-function js() {
-  return gulp
-    .src([
-      './js/*.js',
-      '!./js/*.min.js',
-    ])
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browsersync.stream());
-}
 
-// Watch files
-function watchFiles() {
-  gulp.watch("./scss/**/*", css);
-  gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
-  gulp.watch("./**/*.html", browserSyncReload);
-}
+// Compile less files of starter kit
+gulp.task('less_starters', function() {
+    return gulp
+        .src('assets/less/_main_starters/*.less')     // locate /less/ folder root to grab 4 main files
+        .pipe(less())                                 // compile
+        .pipe(gulp.dest('starters/assets/css'))       // destination path for normal CSS
+        .pipe(minifyCss({                             // minify CSS
+            keepSpecialComments: 0                    // remove all comments
+        }))
+        .pipe(rename({                                // rename file
+            suffix: ".min"                            // add *.min suffix
+        }))
+        .pipe(gulp.dest('starters/assets/css'));      // destination path for minified CSS
+});
 
-// Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
-const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-// Export tasks
-exports.css = css;
-exports.js = js;
-exports.clean = clean;
-exports.vendor = vendor;
-exports.build = build;
-exports.watch = watch;
-exports.default = build;
+// Concatenate & minify JS (uncomment if you want to use)
+/*gulp.task('concatenate', function() {
+    return gulp
+        .src('assets/js/*.js')                        // path to js files you want to concat
+        .pipe(concat('all.js'))                       // output file name
+        .pipe(gulp.dest('assets/js'))                 // destination path for normal JS
+        .pipe(rename({                                // rename file
+            suffix: ".min"                            // add *.min suffix
+        }))
+        .pipe(uglify())                               // compress JS
+        .pipe(gulp.dest('assets/js'));                // destination path for minified JS
+});*/
+
+
+// Minify template's core JS file
+gulp.task('minify_core', function() {
+    return gulp
+        .src('assets/js/core/app.js')                 // path to app.js file
+        .pipe(uglify())                               // compress JS
+        .pipe(rename({                                // rename file
+            suffix: ".min"                            // add *.min suffix
+        }))
+        .pipe(gulp.dest('assets/js/core/'));          // destination path for minified file
+});
+
+
+// Watch files for changes
+gulp.task('watch', function() {
+    gulp.watch('assets/js/core/app.js', [             // listen for changes in app.js file and automatically compress
+        'lint',                                       // lint
+        //'concatenate',                              // concatenate & minify JS files (uncomment if in use)
+        'minify_core'                                 // compress app.js
+    ]); 
+    gulp.watch('assets/less/**/*.less', ['less_full', 'less_starters']);    // listen for changes in all LESS files and automatically re-compile
+});
+
+
+// Default task
+gulp.task('default', [                                // list of default tasks
+    'lint',                                           // lint
+    'less_full',                                      // full version less compile
+    'less_starters',                                  // starter kit less compile
+    //'concatenate',                                  // uncomment if in use
+    'minify_core',                                    // compress app.js
+    'watch'                                           // watch for changes
+]);
