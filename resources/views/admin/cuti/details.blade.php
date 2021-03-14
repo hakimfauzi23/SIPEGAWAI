4e9da1d12bc355cad4d2c0714cc20db5ffcbb9d6
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-03-14 22:36:20 +0700

    Implementasi Template Detail Cuti Menu Admin

--- resources/views/admin/cuti/details.blade.php
+++ resources/views/admin/cuti/details.blade.php
@@ -1,177 +1,135 @@
-@extends('admin.layouts.base')
+@extends('admin.layout.base')
 
-@section('page_title', 'Details Pengajuan Cuti')
+@section('title', 'Details Data Cuti')
 
-@section('content')
-
-    <!-- Breadcrumb -->
-    <nav aria-label="breadcrumb" class="main-breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('cuti.index') }}">List Pengajuan Cuti</a></li>
-            <li class="breadcrumb-item"> {{ 'ID:' . $cuti->id . '-' . $cuti->pegawai->nama }} </li>
-        </ol>
-    </nav>
-    <!-- /Breadcrumb -->
-
-    <div class="row gutters-sm">
-        <div class="col-md-4 mb-3">
-            <div class="card border-left-primary shadow">
-                <div class="card-body">
-                    <div class="d-flex flex-column align-items-center text-center">
-                        @php $path =Storage::url('images/'.$cuti->pegawai->path); @endphp
-                        <img src="{{ url($path) }}" alt="Foto Profil" width="150">
-                        <div class="mt-3">
-                            <p class="h3 mb-1">{{ $cuti->pegawai->jabatan->nm_jabatan }}</p>
-                            <p class="text-muted h5 ">Divisi {{ $cuti->pegawai->divisi->nm_divisi }}</p>
-                        </div>
-                    </div>
-                </div>
+@section('content_header')
+    <div class="page-header page-header-default">
+        <div class="page-header-content">
+            <div class="page-title">
+                <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Cuti</span>
+                    - Details Data Cuti</h4>
             </div>
-            <div class="card border-left-success shadow mt-3">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> ID Pegawai </h6>
-                        <span class="text-secondary">{{ $cuti->pegawai->id }}</span>
-                    </li>
-                </ul>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> Nama Pegawai </h6>
-                        <span class="text-secondary">{{ $cuti->pegawai->nama }}</span>
-                    </li>
-                </ul>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> Email </h6>
-                        <span class="text-secondary">{{ $cuti->pegawai->email }}</span>
-                    </li>
-                </ul>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> Nomor Handphone </h6>
-                        <span class="text-secondary">{{ $cuti->pegawai->no_hp }}</span>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="card border-left-warning shadow mt-3">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                        <h6 class="mb-0"> Lama Kerja </h6>
-                        <span
-                            class="text-secondary">{{ $interval->y . ' Tahun, ' . $interval->m . ' Bulan, ' . $interval->d . ' Hari ' }}</span>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="card mt-3 align-items-center text-center border-0 bg-transparent">
-                <?php $encyrpt = Crypt::encryptString($cuti->id); ?>
-
-                <div class="sm-6">
-                    <a href="{{ route('cuti.edit', $encyrpt) }}" class="btn btn-success">Edit Data Pengajuan</a>
-                    <a href="{{ route('cuti.destroy', $encyrpt) }}" class="btn btn-danger delete-confirm">Hapus
-                        Data</a>
-                </div>
-
-
-            </div>
-
-
-
-
 
         </div>
 
-        <div class="col-md-8">
-            <div class="card border-left-warning shadow mb-3">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h6 class="mb-0">Tipe Cuti </h6>
-                        </div>
-                        <div class="col-sm-6 text-secondary text-right">
-                            {{ $cuti->tipe_cuti }}
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h6 class="mb-0">Tanggal Pengajuan</h6>
-                        </div>
-                        <div class="col-sm-6 text-secondary text-right">
-                            {{ $cuti->tgl_pengajuan }}
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h6 class="mb-0">Tanggal Mulai</h6>
-                        </div>
-                        <div class="col-sm-6 text-secondary text-right">
-                            {{ $cuti->tgl_mulai }}
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h6 class="mb-0">Tanggal Selesai</h6>
-                        </div>
-                        <div class="col-sm-6 text-secondary text-right">
-                            {{ $cuti->tgl_selesai }}
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h6 class="mb-0">Keterangan</h6>
-                        </div>
-                        <div class="col-sm-6 text-secondary text-right">
-                            {{ $cuti->ket }}
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h6 class="mb-0">Status</h6>
-                        </div>
-                        <div class="col-sm-6 text-secondary text-right">
-                            {{ $cuti->status }}
-                        </div>
-                    </div>
+        <div class="breadcrumb-line">
+            <ul class="breadcrumb">
+                <li> <a href="{{ route('cuti.index') }}"> <i class="active icon-home2 position-left"></i> List Data
+                        Cuti
+                    </a>
+                </li>
+                <li class="active">Details Data Cuti ID : {{ $cuti->id }} </li>
+            </ul>
+        </div>
+    </div>
+@endsection
 
-                </div>
-            </div>
-
-            <div class="row gutters-sm">
-                <div class="col-sm-6 mb-3">
-                    <div class="card border-left-danger shadow h-100">
-                        <div class="card-body">
-                            <h6 class="d-flex align-items-center mb-3">Tanggal Ditolak</h6>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6> {{ $cuti->tgl_ditolak }} </h6>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 mb-3">
-                    <div class="card border-left-success shadow h-100">
-                        <div class="card-body">
-                            <h6 class="d-flex align-items-center mb-3">Tanggal Disetujui</h6>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6> {{ $cuti->tgl_disetujui }} </h6>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+@section('content')
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-heading bg-teal">
+                <h5 class="panel-title">Data Cuti</h5>
+                <div class="heading-elements">
+                    <ul class="icons-list">
+                        <li><a data-action="collapse"></a></li>
+                        <li><a data-action="reload"></a></li>
+                        <li><a data-action="close"></a></li>
+                    </ul>
                 </div>
 
             </div>
-
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-xs">
+                        <tr>
+                            <td>Nama Pemohon</td>
+                            <td>:</td>
+                            <td>{{ $cuti->pegawai->nama }}</td>
+                        </tr>
+                        <tr>
+                            <td>Tipe Cuti</td>
+                            <td>:</td>
+                            <td>{{ $cuti->tipe_cuti }}</td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal Pengajuan</td>
+                            <td>:</td>
+                            <td>{{ $cuti->tgl_pengajuan }}</td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal Mulai Cuti</td>
+                            <td>:</td>
+                            <td>{{ $cuti->tgl_mulai }}</td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal Selesai Cuti</td>
+                            <td>:</td>
+                            <td>{{ $cuti->tgl_selesai }}</td>
+                        </tr>
+                        <tr>
+                            <td>Keterangan</td>
+                            <td>:</td>
+                            <td>{{ $cuti->ket }}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-heading bg-teal">
+                <h5 class="panel-title">Detail Proses</h5>
+                <div class="heading-elements">
+                    <ul class="icons-list">
+                        <li><a data-action="collapse"></a></li>
+                        <li><a data-action="reload"></a></li>
+                        <li><a data-action="close"></a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="panel-body">
+                <div class="progress" style="height:40px">
+                    <div <?php if ($cuti->status == 'Diproses') {
+                        echo 'class="progress-bar bg-success"';
+                        } else {
+                        echo 'class="progress-bar bg-success"';
+                        } ?> style="width:33.2%">
+                        Diproses
+                        <br>
+                        {{ $cuti->tgl_pengajuan }}
+                    </div>
+
+                    <div <?php if ($cuti->status == 'Ditolak Atasan') {
+                        echo 'class="progress-bar bg-danger" style="width:33.2%">
+                        Ditolak Atasan <br>' . $cuti->tgl_ditolak_atasan;
+                        } elseif ($cuti->status == 'Disetujui Atasan' || ($cuti->status == 'Disetujui HRD' || $cuti->status
+                        == 'Ditolak HRD')) {
+                        echo 'class="progress-bar bg-success" style="width:33.2%">
+                        Disetujui Atasan <br>' . $cuti->tgl_disetujui_atasan;
+                        } else {
+                        echo 'class="progress-bar" style="width:33.2%">';
+                        } ?>
+                    </div>
+
+                    <div <?php if ($cuti->status == 'Ditolak HRD') {
+                        echo 'class="progress-bar bg-danger" style="width:33.2%">
+                        Ditolak HRD <br>' . $cuti->tgl_ditolak_hrd;
+                        } elseif ($cuti->status == 'Disetujui HRD') {
+                        echo 'class="progress-bar bg-success" style="width:33.2%">
+                        Disetujui HRD <br>' . $cuti->tgl_disetujui_hrd;
+                        } else {
+                        echo 'class="progress-bar " style="width:33.2%">';
+                        } ?>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 @endsection
