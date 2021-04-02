eb9310153998e5c43be05551eb9255257a7789d1
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-04-02 13:05:50 +0700

    Dbbuging Lupa Password

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pegawai;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -19,22 +20,30 @@ class ForgotPasswordController extends Controller
 
   public function postEmail(Request $request)
   {
+
     $request->validate([
       'email' => 'required|email',
     ]);
 
-    $token = Str::random(62);
+    $pegawai = Pegawai::where('email', '=', $request->email)->first();
+    // dd($pegawai);
+    if ($pegawai === null) {
+      Alert::error('error', 'Email Tidak Terdaftar !');
+      return redirect('/forget-password');
+    } else {
+      $token = Str::random(62);
 
-    DB::table('password_resets')->insert(
-      ['email' => $request->email, 'token' => $token, 'created_at' => Carbon::now()]
-    );
+      DB::table('password_resets')->insert(
+        ['email' => $request->email, 'token' => $token, 'created_at' => Carbon::now()]
+      );
 
-    Mail::send('password.verify', ['token' => $token], function ($message) use ($request) {
-      $message->to($request->email);
-      $message->subject('Reset Password Notification');
-    });
+      Mail::send('password.verify', ['token' => $token], function ($message) use ($request) {
+        $message->to($request->email);
+        $message->subject('Reset Password Notification');
+      });
 
-    Alert::success('success', 'link reset anda telah terkirim  apabila email anda terdaftar di database !');
-    return redirect('/');
+      Alert::success('success', 'Link Reset Password Terkirim Di Email Anda !');
+      return redirect('/');
+    }
   }
 }
--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -1,44 +1,53 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
 use App\Models\Pegawai;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use RealRashid\SweetAlert\Facades\Alert;
 
-class ResetPasswordController extends Controller { 
+class ResetPasswordController extends Controller
+{
 
-  public function getPassword($token) { 
+  public function getPassword($token)
+  {
 
-     return view('password.reset', ['token' => $token]);
+    return view('password.reset', ['token' => $token]);
   }
 
   public function updatePassword(Request $request)
   {
 
-  $request->validate([
-      'email' => 'required|email|exists:pegawai',
-      'password' => 'required|string|min:6|confirmed',
-      'password_confirmation' => 'required',
+    // dd($request->token);
 
-  ]);
+    //Cek Validasi Token 
+    $token = DB::table('password_resets')
+      ->where(['email' => $request->email, 'token' => $request->token])
+      ->where('created_at', '>', Carbon::now()->subHours(12))
+      ->first();
 
-  $updatePassword = DB::table('password_resets')
-                      ->where(['email' => $request->email, 'token' => $request->token])
-                      ->first();
+    // dd($token);
+    if ($token == null) {
+      Alert::error('error', 'Token Invalid / Kadaluarsa!');
+      return redirect('/');
+    } else {
+      $request->validate([
+        'email' => 'required|email|exists:pegawai',
+        'password' => 'required|string|min:6|confirmed',
+        'password_confirmation' => 'required',
 
-  if(!$updatePassword)
-      return back()->withInput()->with('error', 'Invalid token!');
+      ]);
 
-    $user = Pegawai::where('email', $request->email)
-                ->update(['password' => Hash::make($request->password)]);
+      $user = Pegawai::where('email', $request->email)
+        ->update(['password' => Hash::make($request->password)]);
 
-    DB::table('password_resets')->where(['email'=> $request->email])->delete();
+      DB::table('password_resets')->where(['email' => $request->email])->delete();
 
-    Alert::success('success', 'Password Anda Berhasil Di Reset!');
-    return redirect('/');
-
+      Alert::success('success', 'Password Anda Berhasil Di Reset!');
+      return redirect('/');
+    }
   }
 }
