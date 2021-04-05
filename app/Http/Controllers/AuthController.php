8809e2cd3537ee119452b983315cdaa9c81569d6
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-04-05 18:53:04 +0700

    make it better

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,6 +40,6 @@ class AuthController extends Controller
     {
         $request->session()->flush();
         Auth::logout();
-        return redirect('/login');
+        return redirect('/');
     }
 }
